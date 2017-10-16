1e1c9804b4448b9de43535b3e880bef7dd243efa
Vishnu Das V R <vishnudasvr07@gmail.com>
2017-10-16 15:12:45 +0530

    leo: Update libshims_ims.
    Change-Id: I913cd4ba657a5931895250bec9dcfe0e7cef74ff

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -36,8 +36,9 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := MediaBuffer.c
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_SRC_FILES := MediaBuffer.cpp
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation libui libgui
+LOCAL_CFLAGS := -Wno-unused-private-field
 LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
