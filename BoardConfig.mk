8a9f1c98a58b20a1545db9cb263c5de556c968f0
GuaiYiHu <guaiyihu@foxmail.com>
2017-10-31 07:35:10 +0800

    leo: Remove TARGET_KERNEL_APPEND_DTB

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,7 +98,6 @@ WCNSS_FILTER_USES_SIBS := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/leo
 TARGET_KERNEL_CONFIG := leo_user_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 androidboot.selinux=permissive
-TARGET_KERNEL_APPEND_DTB := true
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
