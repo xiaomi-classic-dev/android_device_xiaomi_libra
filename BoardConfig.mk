f73a2ff672159229d42ec4555a2152380332b473
Bruno Martins <bgcngm@gmail.com>
2017-12-16 22:09:09 +0000

    leo: Add common compatibility matrix
    Change-Id: I54085a73e48519f1980066c10b54a8783967d362

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -148,6 +148,7 @@ TARGET_PER_MGR_ENABLED := true
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
