f04b99ae4bf15bd1993fc336423f90c39330f737
GuaiYiHu <guaiyihu@foxmail.com>
2018-01-26 16:20:16 +0800

    libra: lineage-ify
    Change-Id: I38002822599b3a922079d3c1322d54b6f38f7d9b

--- lineage.mk
+++ lineage.mk
@@ -6,14 +6,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/libra/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := libra
-PRODUCT_NAME := mk_libra
+PRODUCT_NAME := lineage_libra
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi-4c
 PRODUCT_MANUFACTURER := Xiaomi
