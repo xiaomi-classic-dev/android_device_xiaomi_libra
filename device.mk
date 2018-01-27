9f395a0b03d9c3238c4e57aa390232f46eb10c56
GuaiYiHu <guaiyihu@foxmail.com>
2018-01-27 22:40:06 +0800

    leo: Squash update for ramdisk
    Change-Id: I102e30bafafe5b5cd7de609c8ef0b69d73cd786c

--- device.mk
+++ device.mk
@@ -246,7 +246,6 @@ PRODUCT_MODEL := Mi Note Pro
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.bt.sh \
-    init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
