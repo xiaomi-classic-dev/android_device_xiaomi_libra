dd3fc3ff3899be86ae07c87107235183216b6138
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-02 19:39:50 -0800

    leo: Set shipping API level via inherited makefile
     * PRODUCT_SHIPPING_API_LEVEL is set there
    
    Change-Id: I8adecfbaafb538277f6451a5dbc8abc279bf0c15

--- mk_leo.mk
+++ mk_leo.mk
@@ -4,6 +4,7 @@ PRODUCT_RELEASE_NAME := leo
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
 
 # Inherit some common MK stuff.
 $(call inherit-product, vendor/mk/config/common_full_phone.mk)
