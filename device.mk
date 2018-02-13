2bd358b9899df6d81ca6bb3a01f9a0cb87aed05a
GuaiYiHu <guaiyihu@foxmail.com>
2018-02-13 12:44:27 +0800

    leo: Add android.hardware.drm@1.0-service
    Change-Id: Ic6a6dbb4032a61176b3ecb84a6e133ed5cb87fe9

--- device.mk
+++ device.mk
@@ -188,6 +188,7 @@ PRODUCT_PACKAGES += \
 # OMX
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
     libc2dcolorconvert \
     libdivxdrmdecrypt \
     libdrmclearkeyplugin \
