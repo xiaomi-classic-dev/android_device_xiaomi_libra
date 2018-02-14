adf3c5c782d35021f9ebed49eac1b3e730f1b3e9
GuaiYiHu <guaiyihu@foxmail.com>
2018-02-14 13:44:15 +0800

    leo: link wlan_mac.bin to /data/misc/wifi/wlan_mac.bin
    Change-Id: If0cf6dd621a070a9583bdfaccf50f8b1048d0287

--- Android.mk
+++ Android.mk
@@ -24,7 +24,7 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/qca_cld; \
     ln -sf /system/etc/wifi/WCNSS_qcom_cfg.ini \
 	    $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini; \
-    ln -sf /persist/wlan_mac.bin \
+    ln -sf /data/misc/wifi/wlan_mac.bin \
 	    $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/wlan_mac.bin)
 # END WiFi symlinks
 
