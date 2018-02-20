13d5d622e9fbb5f4edf6d034872f173f03e8a934
GuaiYiHu <guaiyihu@foxmail.com>
2018-02-20 20:39:38 +0800

    leo: audio: Add missing output policy config
    Change-Id: I88bd5b225c49dd1f38cdce7840a7b3a8642a490d

--- device.mk
+++ device.mk
@@ -23,6 +23,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_platform_info_i2s.xml:system/etc/audio_platform_info_i2s.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
