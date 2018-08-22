e0abf4aa10c1bc8314beb1f020d0345c0a1f7ee2
guaiyihu <guaiyihu@foxmail.com>
2018-08-22 23:57:36 +0800

    leo: Move shim libraries to vendor
    Change-Id: I4fac4a10c5e65248c6f968234222dd565d791950

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -18,26 +18,27 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := get_process_name.c
 LOCAL_MODULE := libshims_get_process_name
 LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := atomic.cpp
 LOCAL_MODULE := libshim_atomic
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := rild_socket.c
 LOCAL_MODULE := rild_socket
 LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := MediaBuffer.c
-
 LOCAL_SHARED_LIBRARIES := libstagefright_foundation
-
 LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
-
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
