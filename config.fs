c7163ff4bc1e8da4128bcae8d640199f99d538e8
guaiyihu <guaiyihu@foxmail.com>
2018-08-22 23:19:49 +0800

    leo: Update config.fs
    Change-Id: I57370f70d223fc717cdf59024b95fb2db4fb50b9

--- config.fs
+++ config.fs
@@ -6,3 +6,33 @@ value:2951
 
 [AID_RFS_SHARED]
 value:2952
+
+[vendor/bin/wcnss_filter]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND
+
+[vendor/bin/cnss-daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[vendor/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[vendor/bin/imsdatadaemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[vendor/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE
