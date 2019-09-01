3c50cf9012e0b07142d1d5129ab65d5a7d8c67b2
Bruno Martins <bgcngm@gmail.com>
2019-09-01 14:33:08 +0100

    libra: edgegesture: Migrate to androidx
    Change-Id: I0ccb4eeafe963aa9d30ac19553096a5358131e60

--- edgegesture/src/org/mokee/settings/device/Startup.java
+++ edgegesture/src/org/mokee/settings/device/Startup.java
@@ -23,7 +23,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.SharedPreferences;
-import android.support.v7.preference.PreferenceManager;
+import androidx.preference.PreferenceManager;
 import android.util.Log;
 
 import org.lineageos.internal.util.FileUtils;
