ea816696070163d4e03bb4cd537136f4ef20f655
GuaiYiHu <guaiyihu@foxmail.com>
2019-09-30 07:50:17 +0800

    libra: Move dalvik heap prop to device own dir
    Change-Id: I88eb8f75deb8d5a2b761015697f16a4328ee609e

--- system_prop.mk
+++ system_prop.mk
@@ -2,7 +2,10 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapgrowthlimit=192m \
-    dalvik.vm.heapsize=512m
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480
